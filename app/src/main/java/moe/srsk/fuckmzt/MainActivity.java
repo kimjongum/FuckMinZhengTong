3cf6b5be7f20e4ba2de77e8a674e138ee760b152
ShirosakiMieru <shirosaki@mieru.moe>
2020-12-01 11:15:02 +0800

    update v1.4.0 2020-12-02

--- app/src/main/java/moe/srsk/fuckmzt/MainActivity.java
+++ app/src/main/java/moe/srsk/fuckmzt/MainActivity.java
@@ -1,13 +1,22 @@
 package moe.srsk.fuckmzt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,39 +25,74 @@ public class MainActivity extends AppCompatActivity {
 
     ExitReceiver exitReceiver = new ExitReceiver();
 
+    private static SharedPreferences conf = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //清理v1.3.0及之前版本数据
+        File f = new File(this.getFilesDir(), "data.txt");
+        if (f.exists()) {
+            f.delete();
+        }
+
+        //加载配置
+        conf = PreferenceManager.getDefaultSharedPreferences(this);
+
         IntentFilter filter = new IntentFilter();
         filter.addAction(EXIT_BROADCAST);
         registerReceiver(exitReceiver, filter);
 
-        init();
+        //注册按钮监听
+        findViewById(R.id.btn_click2gen).setOnClickListener(v -> {
+            startQrCodeActivity();
+        });
+        findViewById(R.id.btn_setting).setOnClickListener(v -> {
+            startActivity(new Intent(this, SettingActivity.class));
+        });
+
+
     }
 
-    private void init() {
+    private void startQrCodeActivity() {
+        Intent it = new Intent(this, WebViewActivity.class);
+        it.putExtra("name", conf.getString("name_preference", ""));
+        it.putExtra("id", conf.getString("idcard_preference", ""));
+        startActivity(it);
+    }
 
-        if (!DataUtils.isDataExist(this)) {
-            Toast.makeText(this, "请先设置健康码信息", Toast.LENGTH_LONG).show();
-            Intent intent = new Intent(this, SettingsActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
-        } else {
-            Intent it = new Intent(this, WebViewActivity.class);
-            String[] arr = DataUtils.getData(this);
-            it.putExtra("name", arr[0]);
-            it.putExtra("id", arr[1]);
-            it.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(it);
-        }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return OptionsMenu.onOptionsItemMenuSelected(this, item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.setting_menui, menu);
+        return true;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        init();
+
+        //引导用户设置个人信息
+        Button click2gen = (Button) findViewById(R.id.btn_click2gen);
+        //未设置姓名和身份证
+        if (conf.getString("name_preference", "").isEmpty()
+                || conf.getString("idcard_preference", "").isEmpty()) {
+            Toast.makeText(this, "请先设置您的个人信息！", Toast.LENGTH_LONG).show();
+
+            click2gen.setEnabled(false);
+        } else {
+            click2gen.setEnabled(true);
+            //跳过主页
+            if (conf.getBoolean("skip_mainpage_switch_preference", false)) {
+                startQrCodeActivity();
+            }
+        }
     }
 
     @Override
--- app/src/main/java/moe/srsk/fuckmzt/WebViewActivity.java
+++ app/src/main/java/moe/srsk/fuckmzt/WebViewActivity.java
@@ -1,19 +1,23 @@
 package moe.srsk.fuckmzt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
 
-    String webURL = "file:///android_asset/mzt.html";
+    private String webUrl = "file:///android_asset/mzt.html";
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,13 +25,15 @@ public class WebViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web_view);
 
         Toolbar tb = findViewById(R.id.fake_mzttoolbar);
-        tb.setTitle("<          八闽健康码");
-        setSupportActionBar(tb);
+        tb.setTitle("八闽健康码");
 
+        setSupportActionBar(tb);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         WebView wv = findViewById(R.id.webview);
         wv.getSettings().setAllowFileAccess(true);
-        wv.getSettings().setAllowFileAccessFromFileURLs(true);
+        //wv.getSettings().setAllowFileAccessFromFileURLs(true);
         wv.getSettings().setAllowContentAccess(true);
         wv.getSettings().setJavaScriptEnabled(true);
 
@@ -36,18 +42,29 @@ public class WebViewActivity extends AppCompatActivity {
         String name = ex.getString("name");
         String id = ex.getString("id");
 
-        webURL = webURL + "?name=" + name + "&idcard=" + id;
+        webUrl = webUrl + "?name=" + name + "&idcard=" + id;
 
-        wv.loadUrl(webURL);
+        wv.loadUrl(webUrl);
         wv.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                view.loadUrl(webURL);
+                view.loadUrl(webUrl);
                 return true;
             }
         });
 
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return OptionsMenu.onOptionsItemMenuSelected(this, item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.setting_menui, menu);
+        return true;
     }
 
     @Override
